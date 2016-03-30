dd65384223d7b863e087ca0202bad73f36ce532e
Danny McClanahan <danieldmcclanahan@gmail.com>
2016-03-30 01:49:23 -0500

    add fusion adapt

--- css.hpp
+++ css.hpp
@@ -3,6 +3,8 @@
 
 #include "match.hpp"
 
+#include <boost/spirit/include/phoenix_stl.hpp>
+
 namespace selectree
 {
 namespace css
@@ -134,33 +136,52 @@ negation_arg
   : type_selector | universal | HASH | class | attrib | pseudo
   ;
 */
-std::string generate_matcher(std::string);
+template <typename T>
+match::Matcher<T> generate_matcher(std::string);
 
 namespace tokens
 {
 struct ident {
   std::string str;
 };
+/* clang-format off */
+BOOST_FUSION_ADAPT_STRUCT(ident, (std::string, str))
+/* clang-format on */
 
 struct name {
   std::string str;
 };
+/* clang-format off */
+BOOST_FUSION_ADAPT_STRUCT(name, str)
+/* clang-format on */
 
 struct hash {
   std::string str;
 };
+/* clang-format off */
+BOOST_FUSION_ADAPT_STRUCT(hash, str)
+/* clang-format on */
 
 struct string_tok {
   std::string str;
 };
+/* clang-format off */
+BOOST_FUSION_ADAPT_STRUCT(string_tok, str)
+/* clang-format on */
 
 struct attrib_match_type {
   std::string str;
 };
+/* clang-format off */
+BOOST_FUSION_ADAPT_STRUCT(attrib_match_type, str)
+/* clang-format on */
 
 struct combinator {
   std::string comb;
 };
+/* clang-format off */
+BOOST_FUSION_ADAPT_STRUCT(combinator, str)
+/* clang-format on */
 }
 
 namespace nonterminals
@@ -183,29 +204,49 @@ struct selectors_group : public selector<T> {
 };
 
 template <typename T>
+struct comb_and_sel {
+  tokens::combinator comb;
+  std::unique_ptr<simple_selector<T>> sel;
+};
+/* clang-format off */
+BOOST_FUSION_ADAPT_TPL_STRUCT((T), (comb_and_sel), comb, sel)
+/* clang-format on */
+
+template <typename T>
 struct simple_selector_sequence : public selector<T> {
   std::unique_ptr<simple_selector<T>> first;
-  std::vector<std::tuple<tokens::combinator,
-                         std::unique_ptr<simple_selector<T>>>> simple_selectors;
+  std::vector<comb_and_sel<T>> simp_sels;
   virtual match::Matcher<T> makeMatcher();
 };
+/* clang-format off */
+BOOST_FUSION_ADAPT_TPL_STRUCT((T), (simple_selector_sequence), first, simp_sels)
+/* clang-format on */
 
 template <typename T>
 struct type_selector : public simple_selector<T> {
   tokens::ident element_name;
   virtual match::Matcher<T> makeMatcher();
 };
+/* clang-format off */
+BOOST_FUSION_ADAPT_TPL_STRUCT((T), (type_selector), element_name)
+/* clang-format on */
 
 template <typename T>
 struct universal : public simple_selector<T> {
   virtual match::Matcher<T> makeMatcher();
 };
+/* clang-format off */
+BOOST_FUSION_ADAPT_TPL_STRUCT((T), (universal),)
+/* clang-format on */
 
 template <typename T>
 struct hash : public simple_selector<T> {
   tokens::hash id;
   virtual match::Matcher<T> makeMatcher();
 };
+/* clang-format off */
+BOOST_FUSION_ADAPT_TPL_STRUCT((T), (hash), id)
+/* clang-format on */
 
 /* "class" is a keyword */
 template <typename T>
@@ -213,6 +254,9 @@ struct class_sel : public simple_selector<T> {
   tokens::ident name;
   virtual match::Matcher<T> makeMatcher();
 };
+/* clang-format off */
+BOOST_FUSION_ADAPT_TPL_STRUCT((T), (class_sel), name)
+/* clang-format on */
 
 template <typename T>
 struct attrib_match : public simple_selector<T> {
@@ -221,6 +265,9 @@ struct attrib_match : public simple_selector<T> {
   boost::variant<tokens::ident, tokens::string_tok> val;
   virtual match::Matcher<T> makeMatcher();
 };
+/* clang-format off */
+BOOST_FUSION_ADAPT_TPL_STRUCT((T), (attrib_match), name, match_type, val)
+/* clang-format on */
 
 template <typename T>
 struct pseudo : public selector<T> {
@@ -232,84 +279,125 @@ struct not_pseudo : public pseudo<T> {
   std::unique_ptr<simple_selector<T>> expr;
   virtual match::Matcher<T> makeMatcher();
 };
+/* clang-format off */
+BOOST_FUSION_ADAPT_TPL_STRUCT((T), (not_pseudo), expr)
+/* clang-format on */
 
 struct a_n_plus_b {
   int a;
   int b;
 };
+/* clang-format off */
+BOOST_FUSION_ADAPT_STRUCT(a_n_plus_b, a, b)
+/* clang-format on */
 
 template <typename T>
 struct nth_child : public pseudo<T> {
   a_n_plus_b expr;
   virtual match::Matcher<T> makeMatcher();
 };
+/* clang-format off */
+BOOST_FUSION_ADAPT_TPL_STRUCT((T), (nth_child), expr)
+/* clang-format on */
 
 template <typename T>
 struct nth_last_child : public pseudo<T> {
   a_n_plus_b expr;
   virtual match::Matcher<T> makeMatcher();
 };
+/* clang-format off */
+BOOST_FUSION_ADAPT_TPL_STRUCT((T), (nth_last_child), expr)
+/* clang-format on */
 
 template <typename T>
 struct nth_last_of_type : public pseudo<T> {
   a_n_plus_b expr;
   virtual match::Matcher<T> makeMatcher();
 };
+/* clang-format off */
+BOOST_FUSION_ADAPT_TPL_STRUCT((T), (nth_last_of_type), expr)
+/* clang-format on */
 
 template <typename T>
 struct nth_of_type : public pseudo<T> {
   a_n_plus_b expr;
   virtual match::Matcher<T> makeMatcher();
 };
+/* clang-format off */
+BOOST_FUSION_ADAPT_TPL_STRUCT((T), (nth_of_type), expr)
+/* clang-format on */
 
 template <typename T>
 struct empty : public pseudo<T> {
   virtual match::Matcher<T> makeMatcher();
 };
+/* clang-format off */
+BOOST_FUSION_ADAPT_TPL_STRUCT((T),(empty),)
+/* clang-format on */
 
 template <typename T>
 struct first : public pseudo<T> {
   virtual match::Matcher<T> makeMatcher();
 };
+/* clang-format off */
+BOOST_FUSION_ADAPT_TPL_STRUCT((T), (first),)
+/* clang-format on */
 
 template <typename T>
 struct first_child : public pseudo<T> {
   virtual match::Matcher<T> makeMatcher();
 };
+/* clang-format off */
+BOOST_FUSION_ADAPT_TPL_STRUCT((T), (first_child),)
+/* clang-format on */
 
 template <typename T>
 struct first_of_type : public pseudo<T> {
   virtual match::Matcher<T> makeMatcher();
 };
+/* clang-format off */
+BOOST_FUSION_ADAPT_TPL_STRUCT((T), (first_of_type),)
+/* clang-format on */
 
 template <typename T>
 struct last_child : public pseudo<T> {
   virtual match::Matcher<T> makeMatcher();
 };
+/* clang-format off */
+BOOST_FUSION_ADAPT_TPL_STRUCT((T), (last_child),)
+/* clang-format on */
 
 template <typename T>
 struct last_of_type : public pseudo<T> {
   virtual match::Matcher<T> makeMatcher();
 };
+/* clang-format off */
+BOOST_FUSION_ADAPT_TPL_STRUCT((T), (last_of_type),)
+/* clang-format on */
 
 template <typename T>
 struct only_child : public pseudo<T> {
   virtual match::Matcher<T> makeMatcher();
 };
+/* clang-format off */
+BOOST_FUSION_ADAPT_TPL_STRUCT((T), (only_child),)
+/* clang-format on */
 
 template <typename T>
 struct only_of_type : public pseudo<T> {
   virtual match::Matcher<T> makeMatcher();
 };
+/* clang-format off */
+BOOST_FUSION_ADAPT_TPL_STRUCT((T), (only_of_type),)
+/* clang-format on */
 
 template <typename T>
 struct root : public pseudo<T> {
   virtual match::Matcher<T> makeMatcher();
 };
-
-template <typename T>
-struct {
-};
+/* clang-format off */
+BOOST_FUSION_ADAPT_TPL_STRUCT((T), (root),)
+/* clang-format on */
 }
 }
 }
--- css.tpp
+++ css.tpp
@@ -1,4 +1,4 @@
-#include <sstream>
+#include <boost/spirit/include/qi.hpp>
 
 namespace selectree
 {
